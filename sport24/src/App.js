7934b74e6d87e86f6b5b53e435052d1624a2e195
panos-charalampopoulos <panos10cha@gmail.com>
2022-06-15 23:27:04 +0300

    attempt to add bootstrap

---
+++ sport24/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import NewsList from './components/NewsList';
+import Nav from './components/Nav';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <NewsList />
+    </div>
+  );
+}
+
+export default App;
---
+++ sport24/src/components/NewsList.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import NewsItem from "./NewsItem";
+
+const NewsList = () => {
+    const [articles, setArticles] = useState([]);
+
+    useEffect(() => {
+        const getArticles = async () => {
+            const response = await axios.get('https://newsapi.org/v2/everything?q=football&apiKey=75aee15d7fea48038aee8d5dd15f8657')
+            console.log(response)
+            setArticles(response.data.articles)
+    }
+     getArticles();
+    }, [])
+
+    return (
+        <div>
+            {articles.map(article => {
+                return (
+                <NewsItem 
+                title = {article.title}
+                description = {article.description}
+                url = {article.url}
+                urlToImage = {article.urlToImage}
+                />
+                )
+            })}
+        </div>
+    )
+}
+
+
+export default NewsList;
