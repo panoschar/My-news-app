513f2e3b4cd0eb020d98e155c4035f5b800907b3
panos-charalampopoulos <panos10cha@gmail.com>
2022-06-16 02:12:36 +0300

    layout design

--- sport24/src/components/Nav.js
+++ sport24/src/components/Nav.js
@@ -1,21 +1,22 @@
 import React from "react";
+import "./Nav.css"
 
 function Nav() {
     return (
         <nav class="navbar">
-            <div class="container-fluid">
-                    <a href="https://www.sport24.gr/">
-                        <img src="https://www.sport24.gr/resources/f6b778c/images/logo_sport24.svg" alt="SPORT24" width="163" height="25" />
-                    </a>
-                <a class="nav-link" aria-current="page" href="#">Home</a>
-                <a class="nav-link" href="#">Favorites</a>
-                <a class="nav-link" href="#">Bet</a>
-                <a class="nav-link" href="#">Videos</a>
-                <a class="nav-link" href="#">Match Center</a>
-                <a class="nav-link" href="#">Videos</a>
-                <a class="nav-link" href="#">About Us</a>
-            </div>
-        </nav>
+        <div class="container-fluid">
+                <a href="https://www.sport24.gr/">
+                    <img src="https://www.sport24.gr/resources/f6b778c/images/logo_sport24.svg" alt="SPORT24" width="163" height="25" />
+                </a>
+            <a class="nav-link" aria-current="page" href="#">Home</a>
+            <a class="nav-link" href="#">Favorites</a>
+            <a class="nav-link" href="#">Bet</a>
+            <a class="nav-link" href="#">Videos</a>
+            <a class="nav-link" href="#">Match Center</a>
+            <a class="nav-link" href="#">Videos</a>
+            <a class="nav-link" href="#">About Us</a>
+        </div>
+    </nav>
     );
 };
 
